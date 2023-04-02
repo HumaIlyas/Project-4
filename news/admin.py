44119552a9ffb792790c3c1fdec9eaf3ddc78433
HumaIlyas <hi.wtmconsult@gmail.com>
2023-04-02 08:16:04 +0000

    Add admin for category, post & comment

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
-from .models import Post
+from .models import Category, Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'created_on')
+    list_filter = ('category', 'created_on')
+    search_fields = ('category', 'title')
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
+
+    list_display = ('category', 'title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'category', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'comment', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'comment')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
