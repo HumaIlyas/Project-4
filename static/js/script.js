a4bfa0154c3daf5f5ecb652a522eddf64a6c450e
HumaIlyas <>
2023-04-05 12:58:28 +0000

    Add messages files in account & add messages functionality in script.js, post_detail.html & settings.py

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// Set timeout function for the messages to disappear after some time
+
+setTimeout(function () {
+    let messages = document.getElementById("msg");
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 2500);
