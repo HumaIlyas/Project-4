be9aec81c66b1f054a0c067d351909776573e2ac
HumaIlyas <hi.wtmconsult@gmail.com>
2023-04-23 16:11:50 +0000

    Make changes in base.html to make static files enable at heroku

--- worldnews/settings.py
+++ worldnews/settings.py
@@ -72,7 +72,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,8 +158,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = ('cloudinary_storage.storage'
                        '.StaticHashedCloudinaryStorage')
-STATICFILES_STORAGE = ('whitenoise.storage'
-                       '.CompressedManifestStaticFilesStorage')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
